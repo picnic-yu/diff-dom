e35d3c908d5ae5c033b0d60c7451c3d807014543
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-11-27 15:17:05 +0800

    添加注释

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ let vertualDom = createElement('ul',{class:'list'},[
     createElement('li',{class:'list'},['b']),
     createElement('li',{class:'list'},['c'])
 ])
+// 将虚拟dom转化成真是dom
 let el = render(vertualDom);
 renderDom(el,window.root);
 console.log(el)
 console.log(vertualDom)
+
+
+// DOM diff 比较2个虚拟dom区别  其实就是比较2个对象得区别
+// dom diff 作用  根据2个虚拟对象创建出补丁对象patch，补丁就是描述改变得内容
+// 将这个补丁用来更新dom
